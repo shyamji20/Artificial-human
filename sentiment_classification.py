692ec9d4688f4a694205ee1112809128ef625f06
shyamji20 <31176750+shyamji20@users.noreply.github.com>
2019-04-22 17:25:52 +0530

    file updated

---
+++ sentiment_classification.py
@@ -0,0 +1,183 @@
+import re
+from collections import Counter 
+import numpy as np
+import tensorflow as tf
+
+with open('path of reviews file', 'r') as f:
+    review = f.read()
+reviews = review.split('\n')
+
+with open('path of labels file', 'r') as f:
+    labels = f.read()
+
+# Preprocess the data
+
+def preprocess(raw_review):
+    # The input is a single string (a raw movie review), and 
+    # the output is a single string (a preprocessed movie review)
+    
+    # Remove non-letters        
+    letters_only = re.sub("[^a-zA-Z]", " ", review_text) 
+    
+    # Convert to lower case, split into individual words
+    words = letters_only.lower().split()                         
+    
+    # 6. Join the words back into one string separated by space, 
+    # and return the result.
+    return( " ".join( words ))
+
+print ("Cleaning and parsing the training set movie reviews...\n")
+clean_reviews = []
+for i in range(0, len(reviews)):
+    # If the index is evenly divisible by 1000, print a message
+    if( (i+1)%1000 == 0 ):
+        print ("Review %d of %d\n" % (i+1, num_reviews))                                                                    
+    clean_reviews.append(preprocess(reviews[i]))
+
+all_text =''.join(clean_reviews)
+total_words =all_text.split()
+
+# Convert words to integers
+counts = Counter(words)
+vocab = sorted(counts, key=counts.get, reverse=True)
+vocab_to_int = {word: i for i, word in enumerate(vocab, 1)}
+
+reviews_ints = []
+for each in reviews:
+    reviews_ints.append([vocab_to_int[word] for word in each.split()])
+
+# Truncate each review to 300 steps(same leangth)
+seq_len = 200
+features = np.zeros((len(reviews_ints), seq_len), dtype=int)
+for i, row in enumerate(reviews_ints):
+    features[i, -len(row):] = np.array(row)[:seq_len]
+
+# Training, Validation, Test
+split_idx = int(len(features)*0.8)
+train_x, val_x = features[:split_idx], features[split_idx:]
+train_y, val_y = labels[:split_idx], labels[split_idx:]
+
+test_idx = int(len(val_x)*0.5)
+val_x, test_x = val_x[:test_idx], val_x[test_idx:]
+val_y, test_y = val_y[:test_idx], val_y[test_idx:]
+
+print("\t\t\tFeature Shapes:")
+print("Train set: \t\t{}".format(train_x.shape), 
+      "\nValidation set: \t{}".format(val_x.shape),
+      "\nTest set: \t\t{}".format(test_x.shape))
+
+# Build the Model
+lstm_size = 256             # Number of units in the hidden layers in the LSTM cells
+lstm_layer = 1              # Number of LSTM layers in the network
+batch_size = 500            # The number of reviews to feed the network in one training pass.
+learning_rate = 0.001       # Learning rate
+
+n_words = len(vocab_to_int) + 1 # Adding 1 because we use 0's for padding, dictionary started at 1
+
+# Create the graph object
+graph = tf.Graph()
+# Add nodes to the graph
+with graph.as_default():
+    inputs_ = tf.placeholder(tf.int32, [None, None], name='inputs')
+    labels_ = tf.placeholder(tf.int32, [None, None], name='labels')
+    keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+
+# Embedding Layer
+embed_size = 250 
+
+with graph.as_default():
+    embedding = tf.Variable(tf.random_uniform((n_words, embed_size), -1, 1))
+    embed = tf.nn.embedding_lookup(embedding, inputs_)
+
+# LSTM cell
+with graph.as_default():
+    def lstm():
+    # Your basic LSTM cell
+      lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
+    
+    # Add dropout to the cell
+      drop = tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=keep_prob)
+      return drop
+  
+   # Stack up multiple LSTM layers, for deep learning
+    cell = tf.contrib.rnn.MultiRNNCell([lstm() for _ in range(lstm_layers)])
+    
+   # Getting an initial state of all zeros
+    initial_state = cell.zero_state(batch_size, tf.float32)
+
+# Forward Pass
+with graph.as_default():
+    outputs, final_state = tf.nn.dynamic_rnn(cell, embed,
+                                             initial_state=initial_state)
+
+# Output
+with graph.as_default():
+    predictions = tf.contrib.layers.fully_connected(outputs[:, -1], 1, activation_fn=tf.sigmoid)
+    cost = tf.losses.mean_squared_error(labels_, predictions)
+    
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+
+# Validation accuracy
+with graph.as_default():
+    correct_pred = tf.equal(tf.cast(tf.round(predictions), tf.int32), labels_)
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# Batching
+def get_batches(x, y, batch_size=100):
+    
+    n_batches = len(x)//batch_size
+    x, y = x[:n_batches*batch_size], y[:n_batches*batch_size]
+    for ii in range(0, len(x), batch_size):
+        yield x[ii:ii+batch_size], y[ii:ii+batch_size]
+
+# Train the model
+epochs = 10
+
+with graph.as_default():
+    saver = tf.train.Saver()
+
+with tf.Session(graph=graph) as sess:
+    sess.run(tf.global_variables_initializer())
+    iteration = 1
+    for e in range(epochs):
+        state = sess.run(initial_state)
+        
+        for ii, (x, y) in enumerate(get_batches(train_x, train_y, batch_size), 1):
+            feed = {inputs_: x,
+                    labels_: y[:, None],
+                    keep_prob: 0.5,
+                    initial_state: state}
+            loss, state, _ = sess.run([cost, final_state, optimizer], feed_dict=feed)
+            
+            if iteration%5==0:
+                print("Epoch: {}/{}".format(e, epochs),
+                      "Iteration: {}".format(iteration),
+                      "Train loss: {:.3f}".format(loss))
+
+            if iteration%25==0:
+                val_acc = []
+                val_state = sess.run(cell.zero_state(batch_size, tf.float32))
+                for x, y in get_batches(val_x, val_y, batch_size):
+                    feed = {inputs_: x,
+                            labels_: y[:, None],
+                            keep_prob: 1,
+                            initial_state: val_state}
+                    batch_acc, val_state = sess.run([accuracy, final_state], feed_dict=feed)
+                    val_acc.append(batch_acc)
+                print("Val acc: {:.3f}".format(np.mean(val_acc)))
+            iteration +=1
+    saver.save(sess, "checkpoints/sentiment.ckpt")
+
+# Test the model
+test_acc = []
+with tf.Session(graph=graph) as sess:
+    saver.restore(sess, tf.train.latest_checkpoint('checkpoints'))
+    test_state = sess.run(cell.zero_state(batch_size, tf.float32))
+    for ii, (x, y) in enumerate(get_batches(test_x, test_y, batch_size), 1):
+        feed = {inputs_: x,
+                labels_: y[:, None],
+                keep_prob: 1,
+                initial_state: test_state}
+        batch_acc, test_state = sess.run([accuracy, final_state], feed_dict=feed)
+        test_acc.append(batch_acc)
+    print("Test accuracy: {:.3f}".format(np.mean(test_acc)))
